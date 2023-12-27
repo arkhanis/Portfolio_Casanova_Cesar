d35b907f9d09b8f93a462ad9447734648eb66f55
arkhanis <casanovac@gmail.com>
2023-12-27 11:57:53 -0300

    trabajando en pagina project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 // Dark theme
 
+const body = document.querySelector('body');
+const themeSwitcher = document.querySelector('.theme-switch__checkbox');
+
+themeSwitcher.addEventListener('click', () => {
+  body.classList.toggle('darkMode');
+});
+
+
