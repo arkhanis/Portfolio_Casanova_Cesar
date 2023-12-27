ba2e2d036f4cca5fe4776d9a4c98f8c8d0373964
arkhanis <casanovac@gmail.com>
2023-12-26 23:22:54 -0300

    trabajando en pagina project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,2 @@
-// Portfolio
+// Dark theme
 
-let filterItems = document.querySelectorAll('.portfolio_filters li');
-
-function activePortfolio () {
-    filterItems.forEach (item => {
-        item.classList.remove('.filter-active');
-        this.classList.add('.filter-active');
-        });
-    }
-
-filterItems.forEach (item => {
-    item.addEventListener('click', activePortfolio);
-});
-
-
-// mixitup filter portfolio
-
-let mixer = mixitup('.portfolio_wrap-container', {
-    selectors: {
-        target: '.portfolio_item'
-    },
-    animation: {
-        duration: 300
-    }
-});
